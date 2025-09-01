ff2b7bd0d4391813b2efe85527bbf27c4c581e62
DiegoAvilaEstrada <de@t2mc.net>
2025-08-31 20:52:54 -0600

    add cancel job service.

---
+++ src/main/java/ayd/back/taller/dto/response/JobDto.java
@@ -0,0 +1,25 @@
+package ayd.back.taller.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class JobDto {
+
+    private Integer id;
+
+    @JsonProperty("vehicle_plate")
+    private String vehiclePlate;
+
+    private String description;
+
+    @JsonProperty("estimated_time")
+    private Integer estimatedTime;
+
+    private String status;
+}
--- src/main/java/ayd/back/taller/repository/crud/JobRepository.java
+++ src/main/java/ayd/back/taller/repository/crud/JobRepository.java
@@ -2,9 +2,23 @@ package ayd.back.taller.repository.crud;
 
 import ayd.back.taller.repository.entities.JobEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface JobRepository extends JpaRepository<JobEntity,Integer> {
+
+    @Query(value = "select * from jobs where status = ?;", nativeQuery = true)
+    List<JobEntity> getAllJobsByStatus(String status);
+
+
+    @Transactional
+    @Modifying
+    @Query(value = "update jobs set status = 'CANCELADO' where id = ?;", nativeQuery = true)
+    void cancelJob(String jobId);
 
 }
